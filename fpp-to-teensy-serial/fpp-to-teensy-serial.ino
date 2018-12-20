0669315dfbc77586da1c5df426021aedeb1066d7
Nick Anderson <stuff@nickandbeth.com>
2018-12-19 22:22:54 -0500

    Updates from 2018
    Removed code to switch color order. Use the sequencing software (like
    Vixen) to manage that. Added comments based on forum discussions to help
    with setting up to use this code.

--- fpp-to-teensy-serial/fpp-to-teensy-serial.ino
+++ fpp-to-teensy-serial/fpp-to-teensy-serial.ino
@@ -1,4 +1,8 @@
 // Created by Nick Anderson 7/2/2017
+// Updated for Halloween 2018 Show 10/8/2018
+//  Updated MAX_PIXELS_PER_STRIP to 517
+//  Removed color order changing, will use Vixen/xLight to manage
+// Added comments from forum posts to help with setting up to use this code 12/19/2018
 
 #include <OctoWS2811.h>
 #define LEDPIN 13 // Teensy 3.2 LED Pin
@@ -6,11 +10,20 @@
 // Since the OctoWS2811 library is designed for 8 equal length led strips, we must define the max number of pixels on a single strip.
 // For example, if you plan to run 3 strips of lengths 48, 50, and 75, set the value to 75.
 // See the LED Address & Different Strip Lengths section of https://www.pjrc.com/teensy/td_libs_OctoWS2811.html for more detail.
-#define MAX_PIXELS_PER_STRIP 220
+#define MAX_PIXELS_PER_STRIP 517
+// When you change the MAX_PIXELS_PER_STRIP, be sure to update Vixen/xLight/FPP/etc with the new channel/output count of MAX_PIXELS_PER_STRIP * 8 * 3
 
 // Within the sequencing software such as Vixen or Falcon Pi Player (FPP), the serial connection should be configured for MAX_PIXELS_PER_STRIP * 8 * 3 outputs.
 // 8 for the number of strips and 3 for each of the R, G, and B channels. The mapping of the channels to align with each of the 8 output the OctoWS2811 library should be within the sequencing software.
+// Color order should be configured in the sequencing software.
 // This code simply puts the serial stream into the OctoWS2811's input.
+
+// Under the Arduino Tools menu, make sure the "USB Type" is set to just "Serial". You might also try changing "Optimize" from "Faster" to "Fast".
+
+// When you patch up the controller in Vixen (and likely other programs), you'll end up with unconnected gaps in the outputs from the last used channel of one strip to the Starting Output of the next strip.
+// These gaps are critical to the Teensy code since all it's really doing it taking a stream of data and feeding it to the OctoWS2811 library. I know this adds an extra headache when setting up the channel outputs.
+// You can calculate a strip's Starting Output with ((Strip_Number - 1) * MAX_PIXELS_PER_STRIP * 3) + 1. If you had MAX_PIXELS_PER_STRIP as 444, the start of Strip 3 would be ((3 - 1) * 444 * 3) + 1 = 2665.
+// When connecting the outputs in Vixen for Strip 3, you'd start with channel 2665 on your Generic Serial / Teensy controller.
 
 DMAMEM int displayMemory[MAX_PIXELS_PER_STRIP * 6];
 int drawingMemory[MAX_PIXELS_PER_STRIP * 6];
@@ -25,7 +38,7 @@ void setup() {
   leds.show();
 
   pinMode(LEDPIN, OUTPUT);
-  digitalWrite(LEDPIN, HIGH); // Led on by default, easier to see the Teensy is running.
+  digitalWrite(LEDPIN, HIGH); // Led on by default, easier to see the Teensy is running. Ideally, should always be on. If it goes on and off, it indicates the Serial Header is not where it is expected to be. Check channel/output counts.
 }
 
 int curPixel = -1;
@@ -37,8 +50,6 @@ void loop() {
   // Teensy buffers 2 USB packets, ~128 bytes, but Serial.available only returns the bytes remaining in the first packet,
   // this means multi-byte data could be split between packets, so we'll plan for the worst case.
 
-  // Set the OctoWS2811 config so that the first 4 channels are RGB
-  // Can comment out the next line if all LEDs ues the same color order
   OctoWS2811 leds(MAX_PIXELS_PER_STRIP, displayMemory, drawingMemory, WS2811_RGB | WS2813_800kHz);
 
   // Process serial data until the header <> is found
@@ -69,11 +80,6 @@ void loop() {
       serialBufferCounter = 0;
       leds.setPixel(curPixel++, serialBuffer[0], serialBuffer[1], serialBuffer[2]);
     }
-
-    // Set the OctoWS2811 config so that the second 4 channels are GRB
-    // Can comment out the next 2 lines if all LEDs ues the same color order
-    if (curPixel == (MAX_PIXELS_PER_STRIP * 4))
-      OctoWS2811 leds(MAX_PIXELS_PER_STRIP, displayMemory, drawingMemory, WS2811_GRB | WS2813_800kHz);
   }
   leds.show();
   curPixel = -1;
